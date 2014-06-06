edc7fe8de32d5bf62e27da187c6c7e37413b9b9d
Isaac Lim <isaaclimdc@gmail.com>
2014-06-06 12:18:55 -0700

    Working uploading from file input

--- app/routes/api.js
+++ app/routes/api.js
@@ -28,29 +28,34 @@ module.exports = function(app, passport, s3, fs) {
 
 	// Upload photo
 	app.post(apiPath('/album/:albumID/upload'), function(req, res) {
+		var imageName = req.files.newImage.name
+		var localPath = req.files.newImage.path;
 		var albumID = req.params.albumID;
-		var filePath = req.query.file;
-		var fullFilePath = __dirname+'/'+filePath;
-		var remotePath = assetPath(albumID, filePath);
-		console.log(albumID, fullFilePath, remotePath);
+		var remotePath = assetPath(albumID, imageName);
+		console.log(albumID, localPath, remotePath);
 
-		// Read in the file and store to S3
-		fs.readFile(fullFilePath, function (imgErr, imgData) {
-		  if (imgErr) throw imgErr;
-
-		  s3.client.putObject({
-		    Bucket: BUCKET_NAME,
-		    Key: remotePath,
-		    Body: imgData
-		  }, function (s3Err, s3Data) {
-		    if (s3Err) {
-		    	console.log(s3Err, s3Err.stack);
-		    	res.send(400);
-		    } else {
-		    	console.log('Successfully uploaded file!', s3Data);
-		    	res.send(200);
-		    }
-		  });
+		// Read in the file
+		fs.readFile(localPath, function (imgErr, imgData) {
+			if (imgErr) {
+				console.log("Error reading image!")
+				res.redirect("/");
+				res.send(404);
+			} else {
+				// Send the file to S3
+			  s3.client.putObject({
+			    Bucket: BUCKET_NAME,
+			    Key: remotePath,
+			    Body: imgData
+			  }, function (s3Err, s3Data) {
+			    if (s3Err) {
+			    	console.log(s3Err, s3Err.stack);
+			    	res.send(404);
+			    } else {
+			    	console.log('Successfully uploaded file!', s3Data);
+			    	res.send(200);
+			    }
+			  });
+			}
 		});
 	});
 
