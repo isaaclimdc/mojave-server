ad90e46457eee18c0c9fe0158c3faf0317a4d19b
Isaac Lim <isaaclimdc@gmail.com>
2014-06-05 17:29:50 -0700

    Working upload API using AWS module

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,13 +2,57 @@
 // MOJAVE API ==================================================================
 // =============================================================================
 
-module.exports = function(app, passport, knox) {
+module.exports = function(app, passport, AWS, fs) {
+	function apiPath(arg) {
+		return '/api'+arg
+	}
+
+	function albumPath(albumID) {
+		return 'albums/'+albumID
+	}
+
+	function assetPath(albumID, filename) {
+		return 'albums/'+albumID+'/'+filename;
+	}
+
+	function getFileExt(filename) {
+		var a = filename.split('.');
+		if (a.length === 1 || (a[0] === '' && a.length === 2))
+		  return '';
+		return a.pop();
+	}
+
+	// ALBUM =====================================================================
 
 	// Upload photo
-	app.get('/api/upload', function(req, res) {
-		console.log(JSON.stringify(req.query));
-		res.send(200);
+	app.post(apiPath('/album/:albumID/upload'), function(req, res) {
+		var albumID = req.params.albumID;
+		var filePath = req.query.file;
+		var fullFilePath = __dirname+'/'+filePath;
+		var remotePath = assetPath(albumID, filePath);
+		console.log(albumID, fullFilePath, remotePath);
+
+		// Read in the file and store to S3
+		fs.readFile(fullFilePath, function (err, data) {
+		  if (err) { throw err; }
+
+		  var s3 = new AWS.S3();
+		  console.log(s3);
+		  s3.client.putObject({
+		    Bucket: 'b1.mojavebucket',
+		    Key: remotePath,
+		    Body: data
+		  }, function (s3err, s3data) {
+		    if (err) {
+		    	console.log(err, err.stack);
+		    } else {
+		    	console.log('Successfully uploaded file!', data);
+		    	res.send(200);
+		    }
+		  });
+		});
 	});
+
 };
 
 function isLoggedIn(req, res, next) {
