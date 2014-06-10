9fd9b45de5e7c1e979b7c9a4bd11429c45f0b2bf
Isaac Lim <isaaclimdc@gmail.com>
2014-06-09 18:10:58 -0700

    Better new image upload

--- public/js/singleAlbum.js
+++ public/js/singleAlbum.js
@@ -1,24 +1,50 @@
 $(document).ready(function() {
+  $('input[type=file]').bootstrapFileInput();
+  $('.file-inputs').bootstrapFileInput();
+
   loadAlbum();
+
+  $('#newImageForm').submit(submitNewImg);
 });
+
+function submitNewImg(e) {
+  e.preventDefault();
+
+  var table = $('.albumTable');
+  var albumID = table.attr('id');
+
+  console.log("SUBMITTING IMAGE!!!");
+  $.ajax({
+    url: '/api/album/'+albumID+'/upload',
+    type: 'POST',
+    data: new FormData(this),
+    processData: false,
+    contentType: false,
+    success: function(data) {
+      location.reload();
+    },
+    failure: function(err) {
+      throw err;
+    }
+  });
+}
 
 function loadAlbum() {
   var table = $('.albumTable');
   var albumID = table.attr('id');
-  console.log("ALBUM ID IS", albumID);
+  console.log("ALBUM ID:", albumID);
 
   // Fetch album
-  $.get('/api/album/'+albumID, function (album, status) {
+  $.get('/api/album/'+albumID, function(album, status) {
     // Load title
     $('#singleAlbumTitle').text(album.title);
 
     // Load assets
-    console.log(album.assetThumbs);
     for (var i = 0; i < album.assets.length; i++) {
       var assetID = album.assets[i];
       var thumbURL = album.assetThumbs[i];
 
-      $.get('/api/album/'+albumID+'/'+assetID, function (signedURL, status) {
+      $.get('/api/album/'+albumID+'/'+assetID, function(signedURL, status) {
 
       // <div class="">
       //   <img src="http://lorempixel.com/200/200/">
