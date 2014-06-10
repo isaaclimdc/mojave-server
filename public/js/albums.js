d798898d32a9b10936408997d3984647e2de2b84
Isaac Lim <isaaclimdc@gmail.com>
2014-06-10 14:15:38 -0700

    A modal for new album with working album title

--- public/js/albums.js
+++ public/js/albums.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  $('#newAlbumBtn').click(function(){
-    $.post('/api/album/new', { title: 'My Album' }, function (data, status) {
-      location.reload();
+  $('#newAlbumSaveBtn').click(function() {
+    var albumTitle = $('#albumTitle').val();
+    console.log(albumTitle);
+
+    $.ajax({
+      url: '/api/album/new',
+      type: 'POST',
+      data: { title: albumTitle },
+      success: function(data) {
+        location.reload();
+      },
+      failure: function(err) {
+        throw err;
+      }
     });
   });
 });
